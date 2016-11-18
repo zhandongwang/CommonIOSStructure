2ff0081ba3e1d3b942dead3bd36752ee4948a7fe
fengli <fengli@2dfire.com>
2016-11-18 14:33:21 +0800

    添加PCH

---
+++ CommonStructure/AppLifeCycle/AppMacro.h
@@ -0,0 +1,13 @@
+//
+//  AppMacro.h
+//  CommonStructure
+//
+//  Created by 王战东 on 16/11/18.
+//  Copyright © 2016年 zhandongwang. All rights reserved.
+//
+
+#ifndef AppMacro_h
+#define AppMacro_h
+
+
+#endif /* AppMacro_h */
